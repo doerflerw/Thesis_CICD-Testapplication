648f316740e1df98c6f5530f09f27edcc7791e50
SCHLEISSHEIMER\doerfler <Doerfler@schleissheimer.de>
2020-02-26 07:54:39 +0100

    Add project files.

---
+++ CICD_Example/CICD_Example.cpp
@@ -0,0 +1,12 @@
+﻿// CICD_Example.cpp : Defines the entry point for the application.
+//
+
+#include "CICD_Example.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
